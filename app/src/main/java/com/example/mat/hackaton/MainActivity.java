825797a2e2a7ac23316cb3ca8584f520d1270728
Matus Maliar <matus.maliar@student.tuke.sk>
2018-12-01 12:29:46 +0100

    innitial commit

---
+++ app/src/main/java/com/example/mat/hackaton/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mat.hackaton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int[] pole;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pole = {}
+    }
+}
